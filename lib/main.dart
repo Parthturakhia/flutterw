10712d723f3b5eccb814b78e7452d245d5c7eba9
darshan7041 <darshansolanki0502@gmail.com>
2024-02-23 14:50:40 +0530

    "init "

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Hey(),
+  ));
+}
+
+class Hey extends StatefulWidget {
+  const Hey({super.key});
+
+  @override
+  State<Hey> createState() => _HeyState();
+}
+
+class _HeyState extends State<Hey> {
+  bool valueSwitch=true;
+  bool valueCheck=true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Examples'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Column(
+        children: [
+          Switch(value: valueSwitch, onChanged: (value){
+            setState((){
+
+            });
+            valueSwitch=value;
+
+          }),
+          Checkbox(value: valueCheck, onChanged: (value){
+            setState((){
+              print("hello");
+            });
+            valueCheck=value!;
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+
