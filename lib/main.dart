32f0175f6f4eb12a2f1a95425d2611b1142b32b2
darshan7041 <darshansolanki0502@gmail.com>
2024-02-23 15:56:48 +0530

    "init "

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,17 @@ class Hey extends StatefulWidget {
 class _HeyState extends State<Hey> {
   bool valueSwitch=true;
   bool valueCheck=true;
+  double valueSlider1=50;
+  double valueSlider2=50;
+  double valueSlider3=50;
+
+  Color clr=Colors.white;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: clr,
       appBar: AppBar(
         title: Text('Examples'),
         backgroundColor: Colors.greenAccent,
@@ -28,17 +35,77 @@ class _HeyState extends State<Hey> {
         children: [
           Switch(value: valueSwitch, onChanged: (value){
             setState((){
-
+              if(valueSwitch==true)
+                {
+                  valueSwitch=false;
+                }
+              else{
+                valueSwitch=true;
+              }
             });
             valueSwitch=value;
 
           }),
           Checkbox(value: valueCheck, onChanged: (value){
             setState((){
-              print("sdg"):
+              if(valueSwitch==true)
+              {
+                clr=Colors.green;
+                clr=Colors.blue;
+              }
+              else
+              {
+                clr=Colors.white;
+              }
             });
             valueCheck=value!;
           }),
+          Slider(value: valueSlider1, onChanged: (value){
+            setState(() {
+              if(valueSwitch==true)
+                {
+                  clr=Color.fromRGBO(valueSlider1.toInt(),
+                      valueSlider2.toInt(),
+                      valueSlider3.toInt(), 1.0);
+                }
+              else{
+                  clr=Colors.white;
+              }
+            });
+            valueSlider1=value;
+
+
+          } , min: 0.0, max: 100.0,),
+          Slider(value: valueSlider2, onChanged: (value){
+            setState(() {
+              if(valueSwitch==true)
+              {
+                clr=Color.fromRGBO(valueSlider1.toInt(),
+                    valueSlider2.toInt(),
+                    valueSlider3.toInt(), 1.0);
+              }
+              else{
+                clr=Colors.white;
+              }
+            });
+            valueSlider2=value;
+
+          } , min: 0.0, max: 100.0,),
+          Slider(value: valueSlider3, onChanged: (value){
+            setState(() {
+              if(valueSwitch==true)
+              {
+                clr=Color.fromRGBO(valueSlider1.toInt(),
+                    valueSlider2.toInt(),
+                    valueSlider3.toInt(), 1.0);
+              }
+              else{
+                clr=Colors.white;
+              }
+            });
+            valueSlider3=value;
+
+          } , min: 0.0, max: 100.0,),
         ],
       ),
     );
